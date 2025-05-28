79c69778e0dff98449640521d05bd87031fdacf6
Maria <mgallegod23@fpcoruna.afundacion.org>
2025-05-29 01:28:22 +0200

    Handling errors with axios

--- src/App.js
+++ src/App.js
@@ -5,29 +5,23 @@ const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 export default function App() {
   const [post, setPost] = React.useState(null);
+    const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
-    axios.get(`${baseURL}/1`).then((response) => {
+    axios.get(`${baseURL}/asdf`).then((response) => {
       setPost(response.data);
+    }).catch(error => {
+      setError(error);
     });
   }, []);
 
-  function deletePost() {
-    axios
-      .delete(`${baseURL}/1`)
-      .then(() => {
-        alert("Post deleted!");
-        setPost(null)
-      });
-  }
-
+  if (error) return `Error: ${error.message}`;
   if (!post) return "No post!"
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
-      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
